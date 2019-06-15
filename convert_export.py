b5de15330cbc9ce95c668d6ba297a12a27eae6f3
Bystroushaak <bystrousak@kitakitsune.org>
2019-06-15 23:23:28 +0200

    Added CSS inliner + proper UTF declaration.

--- convert_export.py
+++ convert_export.py
@@ -1,20 +1,36 @@
 #! /usr/bin/env python3
 import os
+import sys
 import shutil
 import os.path
 import zipfile
 import argparse
 
+import dhtmlparser
+
 
 class SharedResources:
-    pass
+    def __init__(self, blog_root):
+        self.css = ""
+        self._css_path = "style.css"
+        self._blog_root = blog_root
+
+    def add_css(self, css):
+        self.css = css
+
+        return self._css_path
+
+    def save(self):
+        with open(os.path.join(self._blog_root, self._css_path), "w") as f:
+            f.write(self.css.strip() + "\n\n")
 
 
 class Page:
-    def __init__(self, path, content, shared):
+    def __init__(self, path: str, content, shared):
         self.path = path
         self.content = content
         self.shared = shared
+        self.dom = dhtmlparser.parseString(self.content)
 
     def save(self, blog_path):
         path = os.path.join(blog_path, self.path)
@@ -24,16 +40,36 @@ class Page:
             os.makedirs(dirname, exist_ok=True)
 
         with open(path, "w") as f:
-            f.write(self.content)
+            f.write(self.dom.prettify())
 
     def postprocess(self, all_pages):
-        pass
+        self._remove_inlined_style(self.dom)
+        self._add_utf_declaration(self.dom)
+
+    def _remove_inlined_style(self, dom):
+        style = dom.match("head", "style")[0]
+
+        style_path = self.shared.add_css(style.getContent())
+        style_path = ("../" * self.path.count("/")) + style_path
+
+        style_str = '<link rel="stylesheet" type="text/css" href="%s">' % style_path
+        new_style = dhtmlparser.parseString(style_str).find("link")[0]
+
+        style.replaceWith(new_style)
+
+    def _add_utf_declaration(self, dom):
+        head = dom.find("head")[0]
+
+        utf_tag_str = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
+        utf_tag = dhtmlparser.parseString(utf_tag_str).find("meta")[0]
+
+        head.childs.append(utf_tag)
 
 
 def generate_blog(zipfile, blog_path):
     remove_old_blog(blog_path)
 
-    shared_resources = SharedResources()
+    shared_resources = SharedResources(blog_path)
 
     all_pages = {}
     for zf, item in iterate_zipfile(zipfile):
@@ -47,6 +83,7 @@ def generate_blog(zipfile, blog_path):
             print(item.filename, "extracted")
 
     postprocess_html(shared_resources, all_pages, blog_path)
+    shared_resources.save()
 
 
 def remove_old_blog(blog_path):
