218d865b6c8c189ad455a2f37fc6e3d68c9df4ae
Bystroushaak <bystrousak@kitakitsune.org>
2019-06-17 16:46:03 +0200

    Implemented twitter card. #7

--- convert_export.py
+++ convert_export.py
@@ -127,6 +127,7 @@ class Page:
         self._add_file_icons(self.dom)
         self._add_breadcrumb(self.dom)
         self._add_patreon_button(self.dom)
+        self._add_twitter_card(self.dom)
 
         full_path_without_filetype = self.path.rsplit(".", 1)[0]
         for path in self.shared.all_pages.keys():
@@ -212,6 +213,60 @@ class Page:
 
         dom.find("body")[0].childs.append(button_tag)
 
+    def _add_twitter_card(self, dom):
+        summary_card = """
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@Bystroushaak" />
+        <meta name="twitter:title" content="{title}" />
+        <meta name="twitter:description" content="{description}" />
+        """
+
+        large_image_card_html = """
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Bystroushaak" />
+        <meta name="twitter:creator" content="@Bystroushaak" />
+        <meta name="twitter:title" content="{title}" />
+        <meta name="twitter:description" content="{description}" />
+        <meta name="twitter:image" content="http://blog.rfox.eu/{image}" />
+        """
+
+        dhtmlparser.makeDoubleLinked(dom)
+
+        p_tags = dom.match(
+            "body",
+            {"tag_name": "div", "params": {"class": "page-body"}},
+            "p"
+        )
+        possible_descriptions = [
+            dhtmlparser.removeTags(p.getContent()) for p in p_tags
+            if not p.find("time") and p.params.get("class") != "column" and \
+            len(dhtmlparser.removeTags(p.getContent())) > 30 and p.parent.params.get("class") == "page-body"
+        ]
+        description = ""
+        if possible_descriptions:
+            description = possible_descriptions[0]
+
+        if not description:
+            return
+
+        if dom.find("img"):
+            first_image_path = dom.find("img")[0].params["src"]
+            full_img_path = os.path.join(os.path.dirname(self.path), first_image_path)
+
+            meta_html = large_image_card_html.format(
+                title=self.title,
+                description=description,
+                image=full_img_path.replace(" ", "%20")
+            )
+        else:
+            meta_html = summary_card.format(
+                title=self.title,
+                description=description,
+            )
+
+        meta_tags = dhtmlparser.parseString(meta_html)
+
+        dom.find("head")[0].childs.extend(meta_tags.find("meta"))
 
 
 def generate_blog(zipfile, blog_root):
