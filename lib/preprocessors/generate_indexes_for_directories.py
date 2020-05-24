19550f8926f4830f10a79e5e2ebf5ab75772942d
Bystroushaak <bystrousak@kitakitsune.org>
2020-05-24 15:57:15 +0200

    GenerateIndexesForDirectories refactored and it now sets .is_index property.

--- lib/preprocessors/generate_indexes_for_directories.py
+++ lib/preprocessors/generate_indexes_for_directories.py
@@ -10,20 +10,33 @@ class GenerateIndexesForDirectories(PreprocessorBase):
     def preprocess(cls, virtual_fs, root):
         settings.logger.info("Generating indexes for directories..")
 
+        for dir, file in cls.all_dirs_that_contain_html_with_same_name(root):
+            index = file.create_copy()
+            index.filename = "index.html"
+
+            dir.files.append(index)
+            index.parent = dir
+
+            # used later to do all kinds of postprocessing
+            file.is_index = True
+            index.is_index = True
+
+    @classmethod
+    def all_dirs_that_contain_html_with_same_name(cls, root):
         for dir in root.walk_dirs():
             dirname = os.path.basename(dir.path).strip()
 
             if not dir.parent:
                 continue
 
-            for file in dir.parent.files:
-                filename = os.path.basename(file.filename)
-                if "." in filename:
-                    filename = filename.rsplit(".", 1)[0].strip()
+            yield from cls._files_with_dirname(dir, dirname)
 
-                if dirname == filename:
-                    index = file.create_copy()
-                    index.filename = "index.html"
+    @classmethod
+    def _files_with_dirname(cls, dir, dirname):
+        for file in dir.parent.files:
+            filename = os.path.basename(file.filename)
+            if "." in filename:
+                filename = filename.rsplit(".", 1)[0].strip()
 
-                    dir.files.append(index)
-                    index.parent = dir
+            if dirname == filename:
+                yield dir, file
