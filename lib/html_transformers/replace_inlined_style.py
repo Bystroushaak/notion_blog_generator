7a2e8a3c95439b3db49cee74940134f70851e351
Bystroushaak <bystrousak@kitakitsune.org>
2020-05-24 22:37:06 +0200

    Removed pre-wrap from CSS.

--- lib/html_transformers/replace_inlined_style.py
+++ lib/html_transformers/replace_inlined_style.py
@@ -31,6 +31,8 @@ class ReplaceInlinedStyles(TransformerBase):
         if cls._initialized:
             return
 
+        original_css = original_css.replace("white-space: pre-wrap;\n", "", 1)
         cls._inlined_style_str = original_css
+
         css_as_bytes = bytes(original_css, "utf-8")
         AddStaticFiles.css.content = css_as_bytes + AddStaticFiles.css.content
