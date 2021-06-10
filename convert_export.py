9d0b1a1934aeb8663c572ec2dda23375e536613f
Bystroushaak <bystrousak@kitakitsune.org>
2021-06-10 18:03:49 +0200

    convert_export.py converted to OOP.

--- convert_export.py
+++ convert_export.py
@@ -3,6 +3,7 @@ import os
 import time
 import shutil
 import argparse
+from typing import Optional
 
 from lib.virtual_fs import VirtualFS
 
@@ -14,55 +15,64 @@ from lib.html_transformers import get_transformers
 from lib.html_transformers.generate_thumbnails import GenerateThumbnails
 
 
-def generate_blog(zipfile, blog_root):
-    start_ts = time.time()
+class BlogGenerator:
+    _blog_tree: Optional[VirtualFS]
 
-    thumb_cache = None
-    if settings.generate_thumbnails:
-        thumb_cache = StoredThumbCache.create_thumb_cache(blog_root)
-        GenerateThumbnails.thumb_cache = thumb_cache
+    def __init__(self, zipfile, blog_root):
+        self._zipfile = zipfile
+        self._blog_root = blog_root
 
-    _make_directory_empty(blog_root)
+        self._blog_tree = None
 
-    blog_tree = VirtualFS(zipfile)
+    @property
+    def _root_node(self):
+        return self._blog_tree.root
 
-    root_node = blog_tree.root
-    _run_preprocessors(blog_tree, root_node)
-    _run_html_transformers(blog_tree, root_node)
-    _run_postprocessors(blog_tree, root_node)
+    def generate_blog(self):
+        start_ts = time.time()
 
-    blog_tree.store_on_disc(blog_root)
+        thumb_cache = None
+        if settings.generate_thumbnails:
+            thumb_cache = StoredThumbCache.create_thumb_cache(self._blog_root)
+            GenerateThumbnails.thumb_cache = thumb_cache
 
-    if thumb_cache:
-        thumb_cache.save_to_storage()
+        _make_directory_empty(self._blog_root)
 
-    settings.logger.info("Blog generated in %.2f seconds.",
-                         time.time() - start_ts)
+        self._blog_tree = VirtualFS(self._zipfile)
 
+        self._run_preprocessors()
+        self._run_html_transformers()
+        self._run_postprocessors()
 
-def _run_preprocessors(blog_tree, root_node):
-    for preprocessor in get_preprocessors():
-        preprocessor.validate_requirements()
-        preprocessor.preprocess(blog_tree, root_node)
-        preprocessor.did_run = True
+        self._blog_tree.store_on_disc(self._blog_root)
 
+        if thumb_cache:
+            thumb_cache.save_to_storage()
 
-def _run_html_transformers(blog_tree, root_node):
-    for transformer in get_transformers():
-        transformer.log_transformer()
-        transformer.validate_requirements()
+        settings.logger.info("Blog generated in %.2f seconds.",
+                             time.time() - start_ts)
 
-        for html_file in root_node.walk_htmls():
-            transformer.transform(blog_tree, root_node, html_file)
+    def _run_preprocessors(self):
+        for preprocessor in get_preprocessors():
+            preprocessor.validate_requirements()
+            preprocessor.preprocess(self._blog_tree, self._root_node)
+            preprocessor.did_run = True
 
-        transformer.did_run = True
+    def _run_html_transformers(self):
+        for transformer in get_transformers():
+            transformer.log_transformer()
+            transformer.validate_requirements()
 
+            for html_file in self._root_node.walk_htmls():
+                transformer.transform(self._blog_tree, self._root_node, html_file)
 
-def _run_postprocessors(blog_tree, root_node):
-    for postprocessor in get_postprocessors():
-        postprocessor.validate_requirements()
-        postprocessor.postprocess(blog_tree, root_node)
-        postprocessor.did_run = True
+            transformer.did_run = True
+
+    def _run_postprocessors(self):
+        for postprocessor in get_postprocessors():
+            postprocessor.validate_requirements()
+            postprocessor.postprocess(self._blog_tree, self._root_node)
+            postprocessor.did_run = True
 
 
 def _make_directory_empty(blog_path):
@@ -109,4 +119,5 @@ if __name__ == '__main__':
     if args.no_thumbs:
         settings.generate_thumbnails = False
 
-    generate_blog(args.zipfile, args.blogroot)
+    generator = BlogGenerator(args.zipfile, args.blogroot)
+    generator.generate_blog()
